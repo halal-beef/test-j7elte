1401372f212fbdf0a45f20f220e8bb1b931bcc3e
smallpp420 <78730004+SmallPP420@users.noreply.github.com>
2021-12-16 18:17:06 +0000

    remove legacy kernel thingy

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -45,9 +45,6 @@ TARGET_FS_CONFIG_GEN := $(LOCAL_PATH)/config.fs
 
 # Kernel
 TARGET_KERNEL_ARCH := arm64
-TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
-KERNEL_TOOLCHAIN := $(BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/aarch64/aarch64-linux-android-4.9/bin
-TARGET_LINUX_KERNEL_VERSION := 3.10
 
 # Kernel config
 TARGET_KERNEL_SOURCE := kernel/samsung/universal7580
